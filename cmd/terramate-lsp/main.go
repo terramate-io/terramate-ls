7c5d7fb63a91ab70d5fb1fc60dec47696d834a7e
Tiago de Bem Natel de Moura <t.nateldemoura@gmail.com>
2022-04-08 11:08:33 +0100

    Update cmd/terramate-lsp/main.go
    Co-authored-by: Tiago Cesar Katcipis <tiagokatcipis@gmail.com>

--- cmd/terramate-lsp/main.go
+++ cmd/terramate-lsp/main.go
@@ -77,7 +77,9 @@ func runServer(conn io.ReadWriteCloser) {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill, syscall.SIGTERM)
 	defer stop()
 
-	logger.Info().Msgf("Starting Terramate Language Server in %s mode ...", *modeFlag)
+	logger.Info().
+		Str("mode", *modeFlag).
+		Msg("Starting Terramate Language Server")
 
 	rpcConn := jsonrpc2.NewConn(jsonrpc2.NewStream(conn))
 	server := tmlsp.NewServer(rpcConn)
