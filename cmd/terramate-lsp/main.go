659e91c621e74f243114d29d6c5a008a5b8d484e
Tiago de Bem Natel de Moura <t.nateldemoura@gmail.com>
2022-04-12 03:40:07 +0100

    feat: add logging. (#2)

--- cmd/terramate-lsp/main.go
+++ cmd/terramate-lsp/main.go
@@ -19,18 +19,34 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"log"
+
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	tmlsp "github.com/mineiros-io/terramate-lsp"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"go.lsp.dev/jsonrpc2"
 )
 
+const (
+	defaultLogLevel = "info"
+	defaultLogFmt   = "console"
+)
+
 var (
-	modeFlag    = flag.String("mode", "stdio", "communication mode (stdio|tcp|websocket)")
-	versionFlag = flag.Bool("version", false, "print version and exit")
+	modeFlag     = flag.String("mode", "stdio", "communication mode (stdio)")
+	versionFlag  = flag.Bool("version", false, "print version and exit")
+	logLevelFlag = flag.String(
+		"log-level", defaultLogLevel,
+		"Log level to use: 'trace', 'debug', 'info', 'warn', 'error', or 'fatal'",
+	)
+	logFmtFlag = flag.String(
+		"log-fmt", defaultLogFmt,
+		"Log format to use: 'console', 'text', or 'json'.",
+	)
 )
 
 func main() {
@@ -44,17 +60,26 @@ func main() {
 	// TODO(i4k): implement other modes.
 	if *modeFlag != "stdio" {
 		fmt.Println("terramate-lsp only supports stdio mode")
-		os.Exit(0)
+		os.Exit(1)
 	}
 
+	configureLogging(*logLevelFlag, *logFmtFlag, os.Stderr)
 	runServer(&readWriter{os.Stdin, os.Stdout})
 }
 
 func runServer(conn io.ReadWriteCloser) {
+	logger := log.With().
+		Str("action", "main.runServer()").
+		Logger()
+
+	logger.Trace().Msg("Creating context for OS signals.")
+
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill, syscall.SIGTERM)
 	defer stop()
 
-	log.Printf("Starting Terramate Language Server in %s mode ...", *modeFlag)
+	logger.Info().
+		Str("mode", *modeFlag).
+		Msg("Starting Terramate Language Server")
 
 	rpcConn := jsonrpc2.NewConn(jsonrpc2.NewStream(conn))
 	server := tmlsp.NewServer(rpcConn)
@@ -69,3 +94,22 @@ type readWriter struct {
 }
 
 func (s *readWriter) Close() error { return nil }
+
+func configureLogging(logLevel string, logFmt string, output io.Writer) {
+	zloglevel, err := zerolog.ParseLevel(logLevel)
+
+	if err != nil {
+		zloglevel = zerolog.FatalLevel
+	}
+
+	zerolog.SetGlobalLevel(zloglevel)
+
+	if logFmt == "json" {
+		zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+		log.Logger = log.Output(output)
+	} else if logFmt == "text" { // no color
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: output, NoColor: true, TimeFormat: time.RFC3339})
+	} else { // default: console mode using color
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: output, NoColor: false, TimeFormat: time.RFC3339})
+	}
+}
