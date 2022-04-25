9843ba4b129d900646a439496d342cfb05eff1f3
Tiago de Bem Natel de Moura <t.nateldemoura@gmail.com>
2022-04-25 23:34:15 +0100

    fix: properly reply to each command and improve tests (#5)

--- lsp_test.go
+++ lsp_test.go
@@ -15,141 +15,36 @@
 package tmlsp_test
 
 import (
-	"context"
 	"encoding/json"
-	"io"
-	"net"
+	"path/filepath"
 	"testing"
 
-	"go.lsp.dev/jsonrpc2"
-	lsp "go.lsp.dev/protocol"
-	"go.lsp.dev/uri"
-
-	"github.com/google/go-cmp/cmp"
 	"github.com/madlambda/spells/assert"
-	tmlsp "github.com/mineiros-io/terramate-lsp"
+	"github.com/mineiros-io/terramate-lsp/test"
 	"github.com/rs/zerolog"
-
-	"github.com/mineiros-io/terramate/test/sandbox"
+	lsp "go.lsp.dev/protocol"
 )
 
 func TestInitialization(t *testing.T) {
-	f := setup(t)
-
-	want := lsp.InitializeResult{
-		Capabilities: lsp.ServerCapabilities{
-			CompletionProvider: &lsp.CompletionOptions{},
-			DefinitionProvider: false,
-			HoverProvider:      false,
-			TextDocumentSync: map[string]interface{}{
-				"change":    float64(1),
-				"openClose": true,
-				"save":      map[string]interface{}{},
-			},
-		},
-	}
-	got := lsp.InitializeResult{}
-
-	_, err := f.editor.call(
-		lsp.MethodInitialize,
-		lsp.InitializeParams{
-			RootURI: uri.File(f.sandbox.RootDir()),
-		},
-		&got)
-
-	assert.NoError(t, err, "calling %q", lsp.MethodInitialize)
-
-	if diff := cmp.Diff(got, want); diff != "" {
-		t.Fatalf("init result differs, got(-) want(+):\n%s", diff)
-	}
-
-	gotReq := <-f.editor.requests
-	assert.EqualStrings(t, lsp.MethodWindowShowMessage, gotReq.Method())
-
-	gotParams := lsp.ShowMessageParams{}
-	assert.NoError(t, json.Unmarshal(gotReq.Params(), &gotParams))
-	if lsp.MessageTypeInfo != gotParams.Type {
-		t.Fatalf("message type got %v != want %v", gotParams.Type, lsp.MessageTypeInfo)
-	}
+	f := test.Setup(t)
+	f.Editor.CheckInitialize()
 }
 
-type fixture struct {
-	sandbox sandbox.S
-	editor  *editor
-}
+func TestDocumentOpen(t *testing.T) {
+	f := test.Setup(t)
 
-func setup(t *testing.T) fixture {
-	t.Helper()
+	stack := f.Sandbox.CreateStack("stack")
+	f.Editor.CheckInitialize()
+	f.Editor.Open("stack/terramate.tm.hcl")
+	r := <-f.Editor.Requests
+	assert.EqualStrings(t, "textDocument/publishDiagnostics", r.Method(),
+		"unexpected notification request")
 
-	// WHY: LSP is bidirectional, the editor calls the server
-	// and the server also calls the editor (not only sending responses),
-	// It is not a classic request/response protocol so we need both
-	// running + connected through a pipe.
-
-	editorRW, serverRW := net.Pipe()
-
-	serverConn := jsonrpc2Conn(serverRW)
-	s := tmlsp.NewServer(serverConn)
-	serverConn.Go(context.Background(), s.Handler)
-
-	editorConn := jsonrpc2Conn(editorRW)
-	e := newEditor(editorConn)
-	editorConn.Go(context.Background(), e.handler)
-
-	t.Cleanup(func() {
-		if err := editorConn.Close(); err != nil {
-			t.Errorf("closing editor connection: %v", err)
-		}
-		if err := serverConn.Close(); err != nil {
-			t.Errorf("closing server connection: %v", err)
-		}
-
-		<-editorConn.Done()
-		<-serverConn.Done()
-
-		// Now that we closed and waited for the editor to stop
-		// we can check that no requests were left unhandled by the test
-		select {
-		case req := <-e.requests:
-			{
-				t.Fatalf("unhandled editor request: %v", req)
-			}
-		default:
-		}
-	})
-
-	return fixture{
-		editor:  e,
-		sandbox: sandbox.New(t),
-	}
-}
-
-type editor struct {
-	conn     jsonrpc2.Conn
-	requests chan jsonrpc2.Request
-}
-
-func newEditor(conn jsonrpc2.Conn) *editor {
-	return &editor{
-		conn:     conn,
-		requests: make(chan jsonrpc2.Request),
-	}
-}
-
-func (e *editor) handler(ctx context.Context, reply jsonrpc2.Replier, r jsonrpc2.Request) error {
-	go func() {
-		e.requests <- r
-	}()
-	return nil
-}
-
-func (e editor) call(method string, params, result interface{}) (jsonrpc2.ID, error) {
-	return e.conn.Call(context.Background(), method, params, result)
-}
-
-func jsonrpc2Conn(rw io.ReadWriteCloser) jsonrpc2.Conn {
-	stream := jsonrpc2.NewStream(rw)
-	return jsonrpc2.NewConn(stream)
+	var params lsp.PublishDiagnosticsParams
+	assert.NoError(t, json.Unmarshal(r.Params(), &params), "unmarshaling params")
+	assert.EqualInts(t, 0, len(params.Diagnostics))
+	assert.EqualStrings(t, filepath.Join(stack.Path(), "terramate.tm.hcl"),
+		params.URI.Filename())
 }
 
 func init() {
