ce33aad77b21f5b6de61cd9cabb90fb6795f8b27
Tiago Natel <t.nateldemoura@gmail.com>
2022-09-21 14:53:51 +0100

    enable logs

--- test/editor.go
+++ test/editor.go
@@ -24,7 +24,6 @@ import (
 	"github.com/google/go-cmp/cmp"
 	"github.com/madlambda/spells/assert"
 	"github.com/mineiros-io/terramate/test/sandbox"
-	"github.com/rs/zerolog"
 	"go.lsp.dev/jsonrpc2"
 	lsp "go.lsp.dev/protocol"
 	"go.lsp.dev/uri"
@@ -153,8 +152,4 @@ func DefaultInitializeResult() lsp.InitializeResult {
 			},
 		},
 	}
-}
-
-func init() {
-	zerolog.SetGlobalLevel(zerolog.Disabled)
 }
